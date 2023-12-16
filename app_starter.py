d7733d2f3e01afa5ec2dec22f144fe49e38266b8
Fixer <67309088+leelang7@users.noreply.github.com>
2023-12-16 23:39:16 +0900

    Add files via upload

---
+++ app_starter.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+from flask_restx import Api, Resource, reqparse
+import os
+import data_from_yf
+
+if __name__ == '__main__':
+    app = Flask(__name__)
+    api = Api(app, version='1.0', title='API 문서', description='Swagger 문서', doc='/api-docs')
+
+    test_api = api.namespace('test', description='Test API') # 콜 받는 주소
+    data = api.namespace('getdata', description='데이터 get API')
+
+    @test_api.route('/')
+    class Test(Resource):
+        def get(self):
+            return "Hello World. This is Test."
+    
+    @data.route('/')
+    class GetData(Resource):
+        def get(self):
+            s = request.args.get('s',1,str)
+            e = request.args.get('e',1,str)
+            print(s, e, type(s))
+            return data_from_yf.getdata(s, e)
+
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 9999)), debug=True)
